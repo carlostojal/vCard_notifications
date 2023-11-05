5d7338d8c81d566bb7057352a21ab89c0e579342
carlostojal <carlos.tojal@hotmail.com>
2023-11-05 13:33:10 +0000

    add a notification dispatcher class

---
+++ src/NotificationDispatcher.js
@@ -0,0 +1,20 @@
+
+class NotificationDispatcher {
+
+    constructor() {
+        // TODO
+    }
+
+    registerClient(user_id, websocket) {
+        // TODO
+    }
+
+    unregisterClient(user_id) {
+        // TODO
+    }
+
+    sendNotification(user_id, notification) {
+        // TODO
+    }
+
+}
