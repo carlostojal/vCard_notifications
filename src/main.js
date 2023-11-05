1e9c35bdd934445fcc2816b7cdd05093860c70fc
carlostojal <carlos.tojal@hotmail.com>
2023-11-05 18:51:10 +0000

    implement websocket authentication

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { WebSocketServer } from 'ws';
 import { createServer, request } from 'http';
 import { Notification, NotificationType } from './Notification.js';
 import NotificationDispatcher from './NotificationDispatcher.js';
+import Authentication from './Authentication.js';
 import 'dotenv/config.js'
 
 // create the http server
@@ -16,15 +17,35 @@ const dispatcher = new NotificationDispatcher();
 // listen for new client connections
 wss.on('connection', (ws, request, client) => {
 
-    // TODO
+    // register the client with the dispatcher
+    dispatcher.registerClient(client, ws);
+
     console.log(`New client ${client} connected`);
 });
 
 // handler when a new socket connection is started
 server.on('upgrade', (request, socket, head) => {
+
+    if(!request.headers.authorization) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.destroy();
+        return;
+    }
     
-    console.log(request);
-    // TODO
+    let user_id;
+    try {
+        // verify the authentication token
+        user_id = Authentication.authenticate(request.headers.authorization);
+    } catch(err) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+        socket.destroy();
+        return;
+    }
+
+    // emit the connection event to the websocket server
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request, user_id);
+    });
 });
 
 server.listen(process.env.WEBSOCKET_SERVER_PORT, () => {
