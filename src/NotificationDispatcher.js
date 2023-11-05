136faf119667b36d0a1abf8db6710e87a162cbdd
carlostojal <carlos.tojal@hotmail.com>
2023-11-05 13:41:18 +0000

    implement the notification dispatcher

--- src/NotificationDispatcher.js
+++ src/NotificationDispatcher.js
@@ -2,19 +2,26 @@
 class NotificationDispatcher {
 
     constructor() {
-        // TODO
+        this._clients = {};
     }
 
     registerClient(user_id, websocket) {
-        // TODO
+        // register a new client to its websocket instance
+        this._clients[user_id] = websocket;
     }
 
     unregisterClient(user_id) {
-        // TODO
+        // unregister a client from its websocket instance
+        delete this._clients[user_id];
+    }
+
+    isClientOnline(user_id) {
+        // check if a client is online
+        return this._clients[user_id] !== undefined;
     }
 
     sendNotification(user_id, notification) {
-        // TODO
+        // send a notification to a client
+        this._clients[user_id].send(JSON.stringify(notification));
     }
-
 }
