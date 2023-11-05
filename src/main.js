5f5703fcc068c5cfd564a29abccbb56b5b8f1927
carlostojal <carlos.tojal@hotmail.com>
2023-11-05 18:33:06 +0000

    start implementing the main file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,32 @@
+import { WebSocketServer } from 'ws';
+import { createServer, request } from 'http';
+import { Notification, NotificationType } from './Notification.js';
+import NotificationDispatcher from './NotificationDispatcher.js';
+import 'dotenv/config.js'
 
-// TODO
+// create the http server
+const server = createServer();
+
+// create the websocket server
+const wss = new WebSocketServer({ noServer: true });
+
+// create the notification dispatcher instance
+const dispatcher = new NotificationDispatcher();
+
+// listen for new client connections
+wss.on('connection', (ws, request, client) => {
+
+    // TODO
+    console.log(`New client ${client} connected`);
+});
+
+// handler when a new socket connection is started
+server.on('upgrade', (request, socket, head) => {
+    
+    console.log(request);
+    // TODO
+});
+
+server.listen(process.env.WEBSOCKET_SERVER_PORT, () => {
+    console.log(`🚀 Server listening on port ${process.env.WEBSOCKET_SERVER_PORT}`);
+});
