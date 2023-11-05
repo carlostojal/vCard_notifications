1980dcd42053e70c517670ceffb8cebadf69ea38
carlostojal <carlos.tojal@hotmail.com>
2023-11-05 13:36:26 +0000

    add the notification type

---
+++ src/Notification.js
@@ -0,0 +1,26 @@
+export const NotificationType = {
+    INFO: "info",
+    CREDIT_IN: "credit_in",
+    CREDIT_OUT: "credit_out",
+    CARD_BLOCK: "card_block"
+}
+
+
+export class Notification {
+
+    constructor(destination, type, message) {
+        // TODO
+    }
+
+    getDestination() {
+        // TODO
+    }
+
+    getType() {
+        // TODO
+    }
+
+    getMessage() {
+        // TODO
+    }
+}
